c6274605e164df2cf564cdc3957af351920a0a1a
izuru775 <izuru775@gmail.com>
2021-10-24 16:57:50 +1100

    Make the guy objects to give cash to each other

--- LendCash/Program.cs
+++ LendCash/Program.cs
@@ -6,7 +6,52 @@ namespace LendCash
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Guy joe = new Guy()
+            {
+                Name = "joe",
+                Cash = 50
+            };
+
+            Guy bob = new Guy()
+            {
+                Name = "bob",
+                Cash = 100
+            };
+
+            while (true)
+            {
+                joe.WriteMyInfo();
+                bob.WriteMyInfo();
+
+                Console.WriteLine("Enter an amount: ");
+                string howMuch = Console.ReadLine();
+
+                if (howMuch == "") return;
+                if (int.TryParse(howMuch,out int result))
+                {
+                    Console.WriteLine("Who should give the cash: ");
+                    string whichGuy = Console.ReadLine();
+
+                    if (whichGuy == "joe")
+                    {
+                        joe.GiveCash(result);
+                        bob.ReceiveCash(result);
+                    }
+                    else if (whichGuy == "bob")
+                    {
+                        bob.GiveCash(result);
+                        joe.ReceiveCash(result);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please Enter 'joe' or 'bob'");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter an amount( or blank line to exit).");
+                }
+            }
         }
     }
 }
