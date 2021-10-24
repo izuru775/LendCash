54e4424a3c0e1942f4bdd90d6a8ebc6ecaf8ee3f
izuru775 <izuru775@gmail.com>
2021-10-24 15:36:37 +1100

    Initial commit

---
+++ LendCash/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LendCash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
